0203e72638e67fe3dc38c8e497894d790f8a938b
Dan <danielmillst@gmail.com>
2017-07-21 00:15:03 -0400

    Registry group

---
+++ fusion-api/src/main/java/org/cyberpwn/fusion/registry/RegistryGroup.java
@@ -0,0 +1,14 @@
+package org.cyberpwn.fusion.registry;
+
+import java.util.List;
+
+public interface RegistryGroup<OBJECT, ID>
+{
+	public Registrar<OBJECT, ID> getRegistrar(String source);
+	
+	public List<String> getRegistrarSources();
+	
+	public List<Registrar<OBJECT, ID>> getRegistrars();
+	
+	public void addRegistrar(Registrar<OBJECT, ID> registrar) throws FusionRegistrationException;
+}
