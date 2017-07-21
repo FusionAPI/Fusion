924db75d69337da727acd40b8cf40c2287fbb693
Dan <danielmillst@gmail.com>
2017-07-21 00:23:34 -0400

    Base conn

--- fusion-api/src/main/java/org/cyberpwn/fusion/Fusion.java
+++ fusion-api/src/main/java/org/cyberpwn/fusion/Fusion.java
@@ -2,5 +2,17 @@ package org.cyberpwn.fusion;
 
 public class Fusion
 {
+	private static Fusion instance;
+	private RegistrarServer registry;
 	
+	public Fusion()
+	{
+		instance = this;
+		registry = new RegistrarServer();
+	}
+	
+	public RegistrarServer getRegistry()
+	{
+		return registry;
+	}
 }
