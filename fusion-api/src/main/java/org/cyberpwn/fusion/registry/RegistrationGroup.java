f5faabac187498807c8f975c9ae9c126af3e4756
Dan <danielmillst@gmail.com>
2017-07-21 00:15:12 -0400

    Registration group functions

---
+++ fusion-api/src/main/java/org/cyberpwn/fusion/registry/RegistrationGroup.java
@@ -0,0 +1,48 @@
+package org.cyberpwn.fusion.registry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RegistrationGroup<OBJECT, ID> implements RegistryGroup<OBJECT, ID>
+{
+	private Map<String, Registrar<OBJECT, ID>> registrars;
+	
+	public RegistrationGroup()
+	{
+		registrars = new HashMap<String, Registrar<OBJECT, ID>>();
+	}
+	
+	public Registrar<OBJECT, ID> getRegistrar(String source)
+	{
+		return registrars.get(source);
+	}
+	
+	public List<String> getRegistrarSources()
+	{
+		return new ArrayList<String>(registrars.keySet());
+	}
+	
+	public List<Registrar<OBJECT, ID>> getRegistrars()
+	{
+		List<Registrar<OBJECT, ID>> reg = new ArrayList<Registrar<OBJECT, ID>>();
+		
+		for(String i : getRegistrarSources())
+		{
+			reg.add(getRegistrar(i));
+		}
+		
+		return reg;
+	}
+	
+	public void addRegistrar(Registrar<OBJECT, ID> registrar) throws FusionRegistrationException
+	{
+		if(getRegistrarSources().contains(registrar.getSource()))
+		{
+			throw new FusionRegistrationException("Registrar " + registrar.getSource() + " already exists");
+		}
+		
+		registrars.put(registrar.getSource(), registrar);
+	}
+}
