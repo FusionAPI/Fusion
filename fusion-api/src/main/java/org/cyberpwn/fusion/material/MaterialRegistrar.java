3fc65b77214b32187d7c8b31cc68d9b52c091fa7
Dan <danielmillst@gmail.com>
2017-07-21 00:15:22 -0400

    Material Registers

---
+++ fusion-api/src/main/java/org/cyberpwn/fusion/material/MaterialRegistrar.java
@@ -0,0 +1,11 @@
+package org.cyberpwn.fusion.material;
+
+import org.cyberpwn.fusion.registry.Registration;
+
+public class MaterialRegistrar extends Registration<String, Integer>
+{
+	public MaterialRegistrar(String source)
+	{
+		super(source);
+	}
+}
