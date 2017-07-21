a39f3356a4f1ee9ef355a78e3b7e34ffa5d9c5d4
Dan <danielmillst@gmail.com>
2017-07-20 21:14:36 -0400

    Fusion exc

---
+++ fusion-api/src/main/java/org/cyberpwn/fusion/FusionException.java
@@ -0,0 +1,11 @@
+package org.cyberpwn.fusion;
+
+public class FusionException extends Exception
+{
+	private static final long serialVersionUID = 3091615570705447655L;
+	
+	public FusionException(String message)
+	{
+		super(message);
+	}
+}
